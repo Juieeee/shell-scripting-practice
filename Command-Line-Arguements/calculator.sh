fd7c8eb3e4d98a3e852fd553afc4d2fdb4395dd9
Jui <jui.vaishnav@gmail.com>
2026-07-18 10:45:22 +0530

    Added Command line arguements practice scripts

---
+++ Command-Line-Arguements/calculator.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Addition= $(($1 + $2))"
+echo "Subtraction: $(($1 -$2))"
+echo "Multiplication: $(($1 * $2))"
+echo "Division: $(($1 / $2))"
+
---
+++ Command-Line-Arguements/voting.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ $1 -ge 18 ]
+then 
+	echo "Eligible for Voting"
+else
+	echo "Not eligible"
+fi
